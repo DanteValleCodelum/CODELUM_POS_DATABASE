2e9bb12c2c1ba2d858792ce92636dd3d81a9f5bb
Dante Valle <dante.valle.m@hotmail.com>
2025-05-29 15:31:18 -0500

    falto la coma

--- db/updates/V2_crear_tabla_comidas.sql
+++ db/updates/V2_crear_tabla_comidas.sql
@@ -1,4 +1,4 @@
 CREATE TABLE comidas (
     id                  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name                VARCHAR(50) NOT NULL,
+    name                VARCHAR(50) NOT NULL
 );
